ca35ea30d50e1f9c8b9a5a281929a00111523420
roqueotha04 <roqueotha04@gmail.com>
2026-03-01 19:24:42 -0300

    feat: finished gateway configuration and centralized all yml files in config server

--- microservice-student/src/main/java/com/microservice/student/controllers/StudentController.java
+++ microservice-student/src/main/java/com/microservice/student/controllers/StudentController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/student")
+@RequestMapping("api/students")
 public class StudentController {
 
     private final StudentService studentService;
